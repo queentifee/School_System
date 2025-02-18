2459fcff25d52f0a7848df308cdbde0edad5f9bd
Queen <samuellqueenie@gmail.com>
2025-02-18 12:47:48 +0100

    changed server url

--- src/swagger.js
+++ src/swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000/",
+                url: "https://school-system-c7r4.onrender.com/",
                 description: 'Development server',
 
             },
